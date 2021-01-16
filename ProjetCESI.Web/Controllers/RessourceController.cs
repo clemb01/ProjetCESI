b85b2e8b9e42e14ae0777c56552ce866cb2a3963
Antoine CACHEUX <coladaitp19-can@ccicampus.fr>
2021-01-16 17:19:36 +0100

    conflit

--- ProjetCESI.Web/Controllers/RessourceController.cs
+++ ProjetCESI.Web/Controllers/RessourceController.cs
@@ -27,16 +27,6 @@ namespace ProjetCESI.Web.Controllers
 
             var ressourceMetier = MetierFactory.CreateRessourceMetier();
 
-            if(model.Ressource.TypeRessourceId == (int)TypeRessources.PDF)
-            {
-                string embed = "<object data=\"{0}\" type=\"application/pdf\" width=\"100%\" height=\"650px\">";
-                embed += "Si vous ne pouvez pas visualiez le fichier, vous pouvez le télécharger <a href = \"{0}\">ici</a>";
-                embed += " ou vous pouvez télécharger <a target = \"_blank\" href = \"http://get.adobe.com/reader/\">Adobe PDF Reader</a> pour visualiser le PDF.";
-                embed += "</object>";
-
-                model.Ressource.Contenu = string.Format(embed, @"/uploads/attestation.pdf");
-            }
-
             return View(model);
         }
 
