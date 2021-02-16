0234f4c3a58c19fa3bf7893ca15b409a790c8014
Clément BOUCHON <coladaitp19-bcl@ccicampus.fr>
2021-02-16 14:43:06 +0100

    Ajout role modo et super admin

--- ProjetCESI.Web/Outils/Helper.cs
+++ ProjetCESI.Web/Outils/Helper.cs
@@ -19,19 +19,61 @@ namespace ProjetCESI.Web.Outils
             await ((TypeRelationMetier)metierFactory.CreateTypeRelationMetier()).GetAll(); // Permet d'initialiser les données
             await ((TypeRessourceMetier)metierFactory.CreateTypeRessourceMetier()).GetAll(); // Permet d'initialiser les données
 
+            var admin = await userManager.FindByNameAsync("Admin");
+            var superAdmin = await userManager.FindByNameAsync("SuperAdmin");
+            var modo = await userManager.FindByNameAsync("Moderateur");
+            var user = await userManager.FindByNameAsync("Citoyen");
 
-            var admins = await userManager.GetUsersInRoleAsync("admin");
-            if (admins.Count == 0)
+            if (superAdmin == null)
             {
-                var admin = new User()
+                superAdmin = new User()
+                {
+                    UserName = "superAdmin",
+                    Email = "super.admin@superadmin.fr",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(superAdmin, "Azerty@153!");
+                await userManager.AddToRoleAsync(superAdmin, Enum.GetName(TypeUtilisateur.SuperAdmin));
+            }
+
+            if (admin == null)
+            {
+                admin = new User()
                 {
                     UserName = "Admin",
                     Email = "admin.admin@admin.fr",
                     EmailConfirmed = true
                 };
 
-                await userManager.CreateAsync(admin, "Admin");
+                await userManager.CreateAsync(admin, "Azerty@153!");
                 await userManager.AddToRoleAsync(admin, Enum.GetName(TypeUtilisateur.Admin));
+            }
+
+            if (modo == null)
+            {
+                modo = new User()
+                {
+                    UserName = "Moderateur",
+                    Email = "modo@modo.fr",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(modo, "Azerty@153!");
+                await userManager.AddToRoleAsync(modo, Enum.GetName(TypeUtilisateur.Moderateur));
+            }
+
+            if (user == null)
+            {
+                user = new User()
+                {
+                    UserName = "Citoyen",
+                    Email = "citoyen@citoyen.fr",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, "Azerty@153!");
+                await userManager.AddToRoleAsync(user, Enum.GetName(TypeUtilisateur.Citoyen));
             }
         }
     }
