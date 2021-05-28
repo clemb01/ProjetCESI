fe15f1435126f22dce901a5c282aec91d51cfde1
Clément BOUCHON <clement.bouchon@gmail.com>
2021-05-28 10:27:07 +0200

    Ajout tests

---
+++ ProjetCESI.Tests/Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProjetCESI.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestBidon()
+        {
+            Assert.Equal(10, 10);
+        }
+    }
+}
